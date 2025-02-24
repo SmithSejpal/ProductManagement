aca531d6e3c9fdf9cd4b39261a840498bb939b4e
SmithSejpal <smith.sejpal007@gmail.com>
2025-02-25 02:47:53 +0530

    Added a comment for ReusableProductId

--- ProductManagement.Core/Entities/ReusableProductId.cs
+++ ProductManagement.Core/Entities/ReusableProductId.cs
@@ -6,6 +6,9 @@ namespace ProductManagement.Core.Entities
     [Table("ReusableProductId")]
     public class ReusableProductId
     {
+        /// <summary>
+        /// Repesents the product ID deleted to be reused in the future.
+        /// </summary>
         [Key]
         [Column(TypeName = "varchar(6)")]
         public string Id { get; set; } = null!;
